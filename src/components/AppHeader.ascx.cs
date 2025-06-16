80ff3dd87c3c84741845df15c8be73ef6d3a39a4
因抽不到SSR而自闭的少年 <2667562244@qq.com>
2025-06-16 13:42:24 +0800

    v0.0 梦开始的地方

---
+++ src/components/AppHeader.ascx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace CodingRep.components
+{
+    public partial class AppHeader : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
