e468c2ed8ef7eec887fb721bb34537c93c85abf1
因抽不到SSR而自闭的少年 <2667562244@qq.com>
2025-06-18 15:35:46 +0800

    V0.1.1.4添加用户注册功能- 新增注册页面 在项目中添加 UserRegisterUtils 类，用于处理用户注册相关逻辑

---
+++ service/UserRegisterUtils.cs
@@ -0,0 +1,7 @@
+namespace CodingRep.service
+{
+    public class UserRegisterUtils
+    {
+        
+    }
+}
--- service/tool/impl/EmailSender.cs
+++ service/tool/impl/EmailSender.cs
@@ -2,39 +2,37 @@ using System;
 using System.Net;
 using System.Net.Mail;
 
-
 namespace CodingRep.service.tool.impl
 {
     public class EmailSender : IEmailSender
     {
-        private readonly String emailAccount = Environment.GetEnvironmentVariable("NETEASE_EMAIL_ACCOUNT");
-        private readonly String authCode     = Environment.GetEnvironmentVariable("NETEASE_EMAIL_AUTH_CODE");
-        public void sendEmail(string to, string subject, string body)
+        private readonly string emailAccount = Environment.GetEnvironmentVariable("NETEASE_EMAIL_ACCOUNT");
+        private readonly string authCode = Environment.GetEnvironmentVariable("NETEASE_EMAIL_AUTH_CODE");
+        private readonly SmtpClient sharedSmtpClient;
+
+        public EmailSender()
         {
-            var client = new SmtpClient("smtp.yeah.net", 25)
+            sharedSmtpClient = new SmtpClient("smtp.yeah.net", 25)
             {
                 Credentials = new NetworkCredential(emailAccount, authCode),
                 EnableSsl = true
             };
+        }
 
+        public void sendEmail(string to, string subject, string body)
+        {
             var mailMessage = new MailMessage(emailAccount, to, subject, body);
-            client.Send(mailMessage);
+            sharedSmtpClient.Send(mailMessage);
         }
 
         public void sendHtmlEmail(string to, string subject, string htmlBody)
         {
-            var client = new SmtpClient("smtp.yeah.net", 25)
-            {
-                Credentials = new NetworkCredential(emailAccount, authCode),
-                EnableSsl = true
-            };
-
             var mailMessage = new MailMessage(emailAccount, to, subject, htmlBody)
             {
                 IsBodyHtml = true
             };
 
-            client.Send(mailMessage);
+            sharedSmtpClient.Send(mailMessage);
         }
     }
 }
